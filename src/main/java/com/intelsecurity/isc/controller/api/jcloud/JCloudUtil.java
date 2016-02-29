744b0165634f12b58f91efaaa6f55a97eb0ce9dc
anadendl <arvind.nadendla@intel.com>
2016-02-29 21:10:48 +0000

    Remove vmidc common dependency from NSC plugin
    git-svn-id: https://ssvn.sc.intel.com/dev/osc/vmidc/trunk/server/NscSdnControllerPlugin@3577 5b5b3ea8-04de-46d7-a81c-c46b85f45ca3

--- src/main/java/com/intelsecurity/isc/controller/api/jcloud/JCloudUtil.java
+++ src/main/java/com/intelsecurity/isc/controller/api/jcloud/JCloudUtil.java
@@ -6,16 +6,17 @@ import java.util.Properties;
 import org.jclouds.Constants;
 import org.jclouds.ContextBuilder;
 
-import com.mcafee.vmidc.rest.client.RestBaseClient;
-
 public class JCloudUtil {
 
     // TODO: Future. Openstack. Externalize the timeout values
     private static final Properties OVERRIDES = new Properties();
 
+    private static final int DEFAULT_READ_TIMEOUT = 60 * 1000;
+    private static final int DEFAULT_CONNECTION_TIMEOUT = 5 * 1000;
+
     static {
-        OVERRIDES.setProperty(Constants.PROPERTY_REQUEST_TIMEOUT, RestBaseClient.DEFAULT_READ_TIMEOUT + "");
-        OVERRIDES.setProperty(Constants.PROPERTY_CONNECTION_TIMEOUT, RestBaseClient.DEFAULT_CONNECTION_TIMEOUT + "");
+        OVERRIDES.setProperty(Constants.PROPERTY_REQUEST_TIMEOUT, DEFAULT_READ_TIMEOUT + "");
+        OVERRIDES.setProperty(Constants.PROPERTY_CONNECTION_TIMEOUT, DEFAULT_CONNECTION_TIMEOUT + "");
         OVERRIDES.setProperty(Constants.PROPERTY_LOGGER_WIRE_LOG_SENSITIVE_INFO, false + "");
         OVERRIDES.setProperty(Constants.PROPERTY_USER_THREADS, 10 + "");
     }
