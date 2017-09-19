13cd0661075aa3f813b0f6f9acb9e35e16794123
Arvind Nadendla <arvind.nadendla@intel.com>
2017-09-19 13:28:50 -0700

    add property which indicates neutron sfc is not supported (#41)
    * add property which indicates neutron sfc is not supported
    
    * fix dependencies
    
    duplicate osgi.core dependency throwing warnings during build
    also move osgi testing dependencies to the end of the dependency list to
    be clear
    
    * address review comments
    
    * fix dependency error

--- src/main/java/org/osc/controller/nsc/api/SampleSdnControllerApi.java
+++ src/main/java/org/osc/controller/nsc/api/SampleSdnControllerApi.java
@@ -49,7 +49,8 @@ import org.osgi.service.transaction.control.jpa.JPAEntityManagerProviderFactory;
                  SUPPORT_FAILURE_POLICY + ":Boolean=false",
                  USE_PROVIDER_CREDS + ":Boolean=true",
                  QUERY_PORT_INFO + ":Boolean=false",
-                 SUPPORT_PORT_GROUP + ":Boolean=false" })
+                 SUPPORT_PORT_GROUP + ":Boolean=false",
+                 SUPPORT_NEUTRON_SFC + ":Boolean=false" })
 public class SampleSdnControllerApi implements SdnControllerApi {
 
     @Reference(target = "(osgi.local.enabled=true)")
